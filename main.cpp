8784fe523aa18ac2fa88d89c06204858cc43813a
J00012 <plumm012@csusm.edu>
2025-01-17 15:51:14 -0800

    this is a working prototype for building cmake

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+
+#include "opencv2/opencv.hpp"
+
+#include <iostream>
+
+int main() {
+    std::string imagePath = "C:/Users/plumm/Downloads/hand_image.jpg";
+
+    cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
+    cv::imshow("Display window", image);
+    cv::waitKey(0);
+    return 0;
+}
