7143d2fd2738cf5e25d9aa1f5f558c7d48512a83
RRefug <99227044+RRefug@users.noreply.github.com>
2025-01-17 17:25:17 -0800

    Just exploring new concepts

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 
 
 #include "opencv2/opencv.hpp"
-
+#include <filesystem> // C++17
 #include <iostream>
 
-int main() {
-    std::string imagePath = "C:/Users/plumm/Downloads/hand_image.jpg";
+namespace fs = std::filesystem;
 
-    cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
+int main() {
+    std::cout << "Current Working Directory: " << fs::current_path() << std::endl;
+
+    std::string imagePath = "C:\\Users\\mrpri\\Desktop\\GestureTrackingApplication\\images\\hand_image.jpg"; //relative path
+    cv::Mat image = cv::imread(imagePath, cv::IMREAD_GRAYSCALE);
+    
+    if (image.empty()) {
+        std::cerr << "**********\nERROR: \nCould not open or find the image!\n**********" << std::endl;
+        return -1;
+    }
+    
     cv::imshow("Display window", image);
     cv::waitKey(0);
     return 0;
