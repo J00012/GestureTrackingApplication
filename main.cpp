53237c79ba1eac5c81bd71b3ced2220e60cc7ff1
J00012 <plumm012@csusm.edu>
2025-01-17 17:24:05 -0800

    here is the final output of mvp for the image output and opencv connection for the cmake

--- main.cpp
+++ main.cpp
@@ -5,9 +5,15 @@
 #include <iostream>
 
 int main() {
-    std::string imagePath = "./images/hand_image.jpg";
+    std::string imagePath = "./hand_image.jpg";
 
+        // Load the image
     cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
+    if (image.empty()) {
+        std::cerr << "Error: Unable to load image at path: " << imagePath << std::endl;
+        return -1;
+    }
+
     cv::imshow("Display window", image);
     cv::waitKey(0);
     return 0;
